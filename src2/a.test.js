59129917d16bf3bbee833fc4425f354ad05c5c9b
Daniel Zlotin <danielzl@wix.com>
2016-11-22 19:29:02 +0200

    stress testing

--- src2/a.test.js
+++ src2/a.test.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import {AppRegistry, Text} from 'react-native';
+import React, {Component} from 'react';
+import renderer from 'react-test-renderer';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <Text>{'Hello, World!'}</Text>
+    );
+  }
+}
 
 describe('something', () => {
   it('should work', () => {
-    console.log(React);
+    AppRegistry.registerComponent('mycomponent', () => MyComponent);
+    const result = renderer.create(
+      <MyComponent/>
+    );
+    expect(result).toBeDefined();
+  });
+
+  it('should work2', () => {
+    AppRegistry.registerComponent('mycomponent', () => MyComponent);
+    const result = renderer.create(
+      <MyComponent/>
+    );
+    expect(result).toBeDefined();
+  });
+
+  it('should work3', () => {
+    AppRegistry.registerComponent('mycomponent', () => MyComponent);
+    const result = renderer.create(
+      <MyComponent/>
+    );
+    expect(result).toBeDefined();
   });
 });
